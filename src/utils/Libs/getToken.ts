ef5ce8a180d8da735aa62e0de48ae4e65666dac9
HWANBINYOO <dngh0825@naver.com>
2023-01-18 17:40:05 +0900

    Add : get,set,remove Token

---
+++ src/utils/Libs/getToken.ts
@@ -0,0 +1,23 @@
+import { GetServerSidePropsContext } from "next";
+import { parseCookies } from "nookies";
+import { tokenReissue } from "../../api/member";
+
+export const getToken = async (ctx:GetServerSidePropsContext|null) => {
+  if(ctx){
+    let Authorization = ctx.req.cookies['Authorization'] || "";
+    let RefreshToken =  ctx.req.cookies['RefreshToken'] || "";
+    
+    if(!Authorization){
+      const {newAuthorization}: any = await tokenReissue(RefreshToken,ctx)
+      Authorization = newAuthorization
+    }
+    
+    return { Authorization, RefreshToken };
+  }
+  else {
+    const {Authorization,RefreshToken} = parseCookies()
+    return { Authorization, RefreshToken }
+  }
+
+
+};

9b4925860e2ca852551999acc47e363970089ca5
HWANBINYOO <dngh0825@naver.com>
2023-01-18 17:23:56 +0900

    Add : common 폴더, common AuthInput

---
+++ src/common/AuthInput/index.tsx
@@ -0,0 +1,19 @@
+import { AuthInputProps } from "../../types";
+
+const AuthInput = ({
+    register,
+    type,
+    placeholder,
+    maxLength,
+  }: AuthInputProps) => {
+    return (
+        <input
+            placeholder={placeholder}
+            {...register}
+            type={type}
+            maxLength={maxLength}
+        />
+    );
+}
+
+export default AuthInput

07a3c740ba8ab8c08eaec02e951bd3481db4e7c3
HWANBINYOO <dngh0825@naver.com>
2023-02-07 22:52:11 +0900

    Add signup,signin page

---
+++ src/pages/signin.tsx
@@ -0,0 +1,12 @@
+import { SignInForm } from "../components";
+import { AuthTemplates } from "../components/Common/templates/AuthTemplates/style";
+
+const SignInPage = () => {
+  return (
+    <AuthTemplates>
+        <SignInForm />
+    </AuthTemplates>
+  )
+}
+
+export default SignInPage

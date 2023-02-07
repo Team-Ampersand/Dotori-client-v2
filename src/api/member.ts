e4a04a9e8953ce163e0a0a9616fe0a9a5ee80e30
HWANBINYOO <dngh0825@naver.com>
2023-02-07 22:23:43 +0900

    Add : Auth api,url 추가

--- src/api/member.ts
+++ src/api/member.ts
@@ -1,7 +1,73 @@
 import { GetServerSidePropsContext } from "next";
+import { toast } from "react-toastify";
 import { apiClient } from "../utils/Libs/apiClient";
-import { TokenController } from "../utils/Libs/requestUrls";
+import { MemberController, TokenController } from "../utils/Libs/requestUrls";
 import { setToken } from "../utils/Libs/setToken";
+
+export const signin = async (id: string, password: string) => {
+	try {
+		const { data } = await apiClient.post(MemberController.signin(), {
+			email: id,
+			password: password,
+		});
+		toast.info('로그인이 되었습니다.')		
+		setToken(data.accessToken, data.refreshToken, null);
+		return { data };
+	} catch (e: any) {
+		if (e.message === 'Request failed with status code 404') {
+			toast.warning('해당 유저가 없어요');
+		} else if (e.message === 'Request failed with status code 400') {
+			toast.info('비밀번호가 올바르지 않아요');
+		}
+	}
+};
+
+export const signup = async (
+	email: string,
+	password: string,
+	name: string,
+	stuNum: number,
+	gender: string
+) => {
+	try {
+		const { data } = await apiClient.post(MemberController.signup(), {
+			email,
+			password,
+			name,
+			stuNum,
+			gender
+		});
+		toast.info('인증되었습니다.')		
+		return { data };
+	} catch (e: any) {
+		if (e.message === 'Request failed with status code 409') {
+			toast.warning('이미 가입된 유저에요');
+		}
+	}
+};
+
+
+export const auth = async (email: string) => {
+	const { data } = await apiClient.post(MemberController.auth(), {
+		email,
+	});
+	return { data };
+};
+
+export const authCheck = async (emailCode: number) => {
+	try {
+		const { data } = await apiClient.post(MemberController.authcheck(), {
+			emailCode: emailCode,
+		});
+		toast.success('인증되었어요.')		
+		return { data };
+	} catch (e: any) {
+		if (e.message === 'Request failed with status code 400') {
+			toast.warning('인증 키가 달라요');
+		}
+	}
+};
+
 
 export const tokenReissue = async (
 	RefreshToken: string,
--- src/utils/Libs/requestUrls.ts
+++ src/utils/Libs/requestUrls.ts
@@ -1,3 +1,23 @@
+export const MemberController = {
+	signin: () => {
+		return '/signin';
+	},
+
+	signup: () => {
+		return '/signup';
+	},
+
+	auth: () => {
+		return '/signup/email';
+	},
+
+	authcheck: () => {
+		return '/signup/email/check';
+	},
+}
+
+
+
 //토큰 재발급
 export const TokenController = {
 	reissue: () => {

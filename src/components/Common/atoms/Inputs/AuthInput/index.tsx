d74c6bd7295cf1cca68a18443a575f4b25cbb5f5
HWANBINYOO <dngh0825@naver.com>
2023-02-07 22:27:50 +0900

    Add : AuthInput

--- src/components/Common/atoms/Inputs/AuthInput/index.tsx
+++ src/components/Common/atoms/Inputs/AuthInput/index.tsx
@@ -1,18 +1,39 @@
+import { LockIcon, PersonIcon, XmarkIcon } from "../../../../../../public/svg";
+import { Palette } from "../../../../../styles/globals";
 import { AuthInputProps } from "../../../../../types";
+import * as S from "./style";
 
 const AuthInput = ({
     register,
     type,
     placeholder,
     maxLength,
-  }: AuthInputProps) => {
+    labelName,
+    isFocus,
+    isValue,
+    handleFocus,
+    DeleteBtnClick,
+    isEmailAuth,
+  }: AuthInputProps) => { 
     return (
-        <input
-            placeholder={placeholder}
-            {...register}
-            type={type}
-            maxLength={maxLength}
-        />
+        <S.AuthInput isRabel={labelName? true:false} isEmailAuth={isEmailAuth? true:false}>
+            <label>
+            {
+                labelName === "persen" ? <PersonIcon color={isFocus ? `${Palette.NEUTRAL_N10}` : `${Palette.NEUTRAL_N30}`}/> : 
+                labelName === "lock" ? <LockIcon color={isFocus ? `${Palette.NEUTRAL_N10}` : `${Palette.NEUTRAL_N30}`}/> : ""
+            }
+            <input
+                placeholder={placeholder}
+                {...register}
+                type={type}
+                maxLength={maxLength}
+                onFocus={handleFocus}
+            />
+            </label>
+            {
+                isValue && <XmarkIcon onClick={DeleteBtnClick}  color={`${Palette.NEUTRAL_N30}`}/>
+            }
+        </S.AuthInput>
     );
 }
 

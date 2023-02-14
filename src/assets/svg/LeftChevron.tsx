430dac9d99e4d23c21ca8741138aadd8f302055b
HWANBINYOO <dngh0825@naver.com>
2023-02-15 08:21:58 +0900

    Add : svg, png 추가

---
+++ src/assets/svg/LeftChevron.tsx
@@ -0,0 +1,9 @@
+import { Palette } from "styles/globals"
+
+const LeftChevron = () => (
+    <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M1 1L5.82322 5.82322C5.92085 5.92085 5.92085 6.07915 5.82322 6.17678L1 11" stroke={Palette.NEUTRAL_N20} strokeWidth="2" strokeLinecap="round"/>
+    </svg>
+)
+
+export default LeftChevron

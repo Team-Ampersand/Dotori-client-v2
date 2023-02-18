9f85d904ea0d3038b5953e38587d6efe221cf954
HWANBINYOO <dngh0825@naver.com>
2023-02-18 15:37:39 +0900

    Add : ChangePasswdPage

---
+++ src/pages/changePasswd.tsx
@@ -0,0 +1,12 @@
+import ChangePasswdForm from "components/ChangePasswd/organisms/ChangePasswdForm";
+import { AuthTemplates } from "components/Common/templates/AuthTemplates/style";
+
+const ChangePasswdPage = () => {
+  return (
+    <AuthTemplates>
+        <ChangePasswdForm/>
+    </AuthTemplates>
+  )
+}
+
+export default ChangePasswdPage

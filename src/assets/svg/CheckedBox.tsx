deffced1603a040d196eddfc5712d23a9f984c87
Jeongmin <srm0609@naver.com>
2023-03-02 12:04:55 +0900

    fix: svg color

--- src/assets/svg/CheckedBox.tsx
+++ src/assets/svg/CheckedBox.tsx
@@ -1,4 +1,10 @@
+import { Palette } from 'styles/globals';
+import { darkMode } from 'recoilAtoms/recoilAtomContainer';
+import { useRecoilState } from 'recoil';
+
 const CheckedBox = () => {
+	const [darkModeState] = useRecoilState(darkMode);
+
 	return (
 		<svg
 			width="24"
@@ -9,13 +15,13 @@ const CheckedBox = () => {
 		>
 			<path
 				d="M23 12C23 16.3923 22.228 19.0427 20.6353 20.6353C19.0427 22.228 16.3923 23 12 23C7.6077 23 4.95735 22.228 3.36468 20.6353C1.77202 19.0427 1 16.3923 1 12C1 7.6077 1.77202 4.95735 3.36468 3.36468C4.95735 1.77202 7.6077 1 12 1C16.3923 1 19.0427 1.77202 20.6353 3.36468C22.228 4.95735 23 7.6077 23 12Z"
-				fill="#6F7AEC"
-				stroke="#6F7AEC"
+				fill={Palette.PRIMARY_P10}
+				stroke={Palette.PRIMARY_P10}
 				strokeWidth="2"
 			/>
 			<path
 				d="M8 12L11 15L16 9"
-				stroke="#FDFDFD"
+				stroke={darkModeState === 'light' ? '#FDFDFD' : '#CDCDD5'}
 				strokeWidth="2"
 				strokeLinecap="round"
 				strokeLinejoin="round"

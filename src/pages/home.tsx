14bdcc7f9dd592e3ca344d713dd9c934c83033bf
HWANBINYOO <dngh0825@naver.com>
2023-03-02 19:42:23 +0900

    Update : HomePage

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,21 +3,25 @@ import { MainTemplates } from 'components/Common/templates/MainTemplates/style'
 import { TimeBoard, Profile, NoticeBoard, SelfStudyBoard, MassageBoard, MealBoard } from 'components/Home/organisms'
 import { HomeTemplates } from 'components/Home/templates/HomeTemplates/style'
 import UseThemeEffect from 'hooks/useThemeEffect'
+import ChannelIOScript from 'components/Home/atoms/Script/SomeScript'
 
 const HomePage = () => {
   UseThemeEffect()
   return (
-    <MainTemplates>
-      <SideBar/>
-      <HomeTemplates>
-        <TimeBoard/>
-        <Profile/>
-        <MealBoard/>
-        <NoticeBoard/>
-        <SelfStudyBoard/>
-        <MassageBoard/>
-      </HomeTemplates>
-   </MainTemplates>
+    <>
+      <MainTemplates>
+        <SideBar/>
+        <HomeTemplates>
+          <TimeBoard/>
+          <Profile/>
+          <MealBoard/>
+          <NoticeBoard/>
+          <SelfStudyBoard/>
+          <MassageBoard/>
+        </HomeTemplates>
+      </MainTemplates>
+      <ChannelIOScript stunumName={'3308유환빈'} email={'s21067@gsm.hs.kr'}/>
+    </>
   )
 }
 

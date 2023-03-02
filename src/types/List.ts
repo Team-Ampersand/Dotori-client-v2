eb0d8f5b84af6bd43694371cb661082b2b0699ec
Jeongmin <srm0609@naver.com>
2023-03-02 10:36:58 +0900

    add: List types

---
+++ src/types/List.ts
@@ -0,0 +1,16 @@
+export interface SelfStudyListProps {
+	index: number;
+	selfStudyCheck: boolean;
+	memberName: string;
+	gender: string;
+	stuNum: string;
+	listType: string;
+}
+
+export interface MassageListProps {
+	index: number;
+	memberName: string;
+	gender: string;
+	stuNum: string;
+	listType: string;
+}

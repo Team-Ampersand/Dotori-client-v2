a9c7ea7776d53d57d9c4106c6423250637bd9616
HWANBINYOO <dngh0825@naver.com>
2023-03-06 13:22:15 +0900

    Update : MenuOption 반응형

--- src/components/Home/atoms/MenuOption/index.tsx
+++ src/components/Home/atoms/MenuOption/index.tsx
@@ -1,8 +1,12 @@
 import { MenuOptionProps } from "types";
+import { ChangeProfileModalcon } from "utils/ChangeProfileModalcon";
 import * as S from "./style";
 
 const MenuOption = ({name, onClick}:MenuOptionProps) => (
     <S.MenuOptionWrapper onClick={onClick} name={name}>
+        <>
+            {ChangeProfileModalcon()[name]()}
+        </>
         {name}
     </S.MenuOptionWrapper>
 )

e56ffa3a9e88187aef4f3dd45c679500bcda0358
Jeongmin <srm0609@naver.com>
2023-03-07 15:18:08 +0900

    add: PenaltyListModal

---
+++ src/components/Penalty/molecules/PenaltyListModal/style.ts
@@ -0,0 +1,38 @@
+import styled from '@emotion/styled';
+import { Palette } from 'styles/globals';
+import { PenaltyItemsStyleProps } from 'types';
+
+export const PenaltyListModalWrapper = styled.div`
+  width: 420px;
+  height: ${(props: PenaltyItemsStyleProps) =>
+    props.itemCnt >= 8 ? '610px' : '430px'};
+  background: ${Palette.BACKGROUND_CARD};
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  z-index: 10;
+  padding: 18px;
+  border-radius: 16px;
+  box-shadow: 0px 8px 24px rgba(0, 0, 0, 0.04);
+`;
+
+export const PenaltyItems = styled.div`
+  height: ${(props: PenaltyItemsStyleProps) =>
+    props.itemCnt >= 8 ? '500px' : '310px'};
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  overflow-y: auto;
+  margin: 15px 0 10px 0;
+`;
+
+export const PenaltyModalBtn = styled.button`
+  height: 45px;
+  background: ${Palette.PRIMARY_P10};
+  border: none;
+  color: white;
+  cursor: pointer;
+  border-radius: 8px;
+  font-weight: 700;
+  font-size: 16px;
+`;

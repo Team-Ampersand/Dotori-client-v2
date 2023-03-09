835be6e05002193a2bff8637f5653b73466ac161
HWANBINYOO <dngh0825@naver.com>
2023-03-09 16:53:45 +0900

    ApplyModifyModal

---
+++ src/components/Home/molecules/ApplyModifyModal/index.tsx
@@ -0,0 +1,39 @@
+import { AuthInput } from "components/Common";
+import ModalHeader from "components/Common/atoms/ModalHeader";
+import { ModalOverayWrapper } from "components/Home/atoms/Wrapper/style";
+import { useForm } from "react-hook-form";
+import { ApplyModifyModalProps } from "types";
+import { isNotNull } from "utils/isNotNull";
+import * as S from "./style";
+
+const ApplyModifyModal = ({modalState, setModalState, name, onClick, maxCount}:ApplyModifyModalProps) => {
+    const { register, watch, resetField } = useForm<{num:number}>({
+        defaultValues: {
+          num: maxCount,
+        },
+    });
+
+    return (
+        <ModalOverayWrapper isClick={modalState}>
+            <S.ApplyModifyModalWrapper>
+                <ModalHeader name={`${name} 인원 수정`} setState={setModalState} />
+                <div>
+                    <p>{`${name} 인원`}</p>
+                    <AuthInput
+                        register={register("num")}
+                        type="number"
+                        placeholder={`${maxCount}`}
+                        DeleteBtnClick={() => resetField('num')}
+                        isValue={isNotNull(watch('num').toString().replace(`${maxCount}`,''))}
+                    />
+                </div>
+                <S.BtnWrapper>
+                    <S.CancelBtn onClick={() => setModalState(false)}>취소</S.CancelBtn>
+                    <S.CheckBtn onClick={() => onClick(watch('num'))}>다음</S.CheckBtn>
+                </S.BtnWrapper>
+            </S.ApplyModifyModalWrapper>
+        </ModalOverayWrapper>
+    )
+}
+
+export default ApplyModifyModal;

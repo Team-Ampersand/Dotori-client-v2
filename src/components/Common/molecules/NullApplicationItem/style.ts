f405f645be96e4db0fad08d4684531780086d364
Jeongmin <srm0609@naver.com>
2023-03-10 14:43:58 +0900

    update: NullApplicationItem 반응형

--- src/components/Common/molecules/NullApplicationItem/style.ts
+++ src/components/Common/molecules/NullApplicationItem/style.ts
@@ -2,7 +2,7 @@ import styled from '@emotion/styled';
 import { Palette } from 'styles/globals';
 
 export const NullApplicstionWrapper = styled.div`
-  width: 80%;
+  width: 100%;
   height: 80%;
   display: flex;
   flex-direction: column;
@@ -18,5 +18,26 @@ export const NullApplicstionWrapper = styled.div`
     color: ${Palette.NEUTRAL_N20};
     font-weight: 500;
     font-size: 16px;
+  }
+  svg {
+    width: 120px;
+  }
+  @media (max-width: 420px) {
+    position: fixed;
+    bottom: 50vh;
+    left: 50vw;
+    transform: translate(-50%, 50%);
+    span {
+      margin-top: 2vh;
+      font-weight: 700;
+      font-size: 16px;
+    }
+    p {
+      font-weight: 500;
+      font-size: 12px;
+    }
+    svg {
+      width: 120px;
+    }
   }
 `;

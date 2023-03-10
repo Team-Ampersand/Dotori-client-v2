50ce1d34824ef3b16389d492cda68a9b2df1afa0
Jeongmin <srm0609@naver.com>
2023-03-10 14:40:17 +0900

    fix: ModalOverayWrapper z-index
    z-index 2로 수정

--- src/components/Common/atoms/Wrappers/ModalOverayWrapper/style.ts
+++ src/components/Common/atoms/Wrappers/ModalOverayWrapper/style.ts
@@ -1,16 +1,17 @@
-import styled from "@emotion/styled";
-import { ModalStyleProps } from "types";
+import styled from '@emotion/styled';
+import { ModalStyleProps } from 'types';
 
 export const ModalOverayWrapper = styled.div`
-    visibility: ${(props:ModalStyleProps) => (props.isClick ? 'visible' : 'hidden')};
-    position: fixed;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    right: 0;
-    z-index: 1;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background: rgba(6, 9, 45, 0.25);
+  visibility: ${(props: ModalStyleProps) =>
+    props.isClick ? 'visible' : 'hidden'};
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  z-index: 2;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: rgba(6, 9, 45, 0.25);
 `;

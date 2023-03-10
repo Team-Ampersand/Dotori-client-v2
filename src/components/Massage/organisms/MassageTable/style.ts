4ecb40aa267aae3082a4ee535144ea1de5fb6df9
Jeongmin <srm0609@naver.com>
2023-03-10 14:47:43 +0900

    update: MassageTable 반응형

--- src/components/Massage/organisms/MassageTable/style.ts
+++ src/components/Massage/organisms/MassageTable/style.ts
@@ -1,16 +1,22 @@
 import styled from '@emotion/styled';
+import { Palette } from 'styles/globals';
 
 export const TableWrapper = styled.div`
   height: 87.5vh;
-  display: flex;
-  justify-content: space-between;
-  margin-top: 24px;
+  @media (max-width: 950px) {
+  }
+  @media (max-width: 420px) {
+    height: auto;
+  }
 `;
 
 export const ListWrapper = styled.div`
+  width: 100%;
   height: 100%;
   display: grid;
-  grid-template-columns: repeat(5, 1fr);
+  grid-template-columns: 200px 200px 200px 200px 200px;
+  grid-template-columns: repeat(5, 210px);
+  grid-auto-rows: 140px;
   grid-column-gap: 20px;
   grid-row-gap: 20px;
   overflow-y: scroll;
@@ -18,5 +24,53 @@ export const ListWrapper = styled.div`
   scrollbar-width: none;
   ::-webkit-scrollbar {
     display: none;
+  }
+  ::-webkit-scrollbar {
+    width: 5px;
+  }
+  ::-webkit-scrollbar-thumb {
+    background: ${Palette.NEUTRAL_N30};
+    border-radius: 10px;
+  }
+  ::-webkit-scrollbar-track {
+    background: ${Palette.NEUTRAL_N50};
+  }
+
+  @media (max-width: 1200px) {
+    grid-template-columns: repeat(4, 1fr);
+  }
+  @media (max-width: 950px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+  @media (max-width: 700px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+  @media (max-width: 420px) {
+    grid-template-columns: repeat(1, 1fr);
+    grid-auto-rows: 170px;
+
+    padding: 0 0 20px;
+  }
+`;
+
+export const ResponseHeader = styled.div`
+  width: calc(100% + 6vw);
+  margin-left: -3vw;
+  display: none;
+  font-weight: 700;
+  font-size: 18px;
+  color: ${Palette.NEUTRAL_N10};
+  justify-content: space-between;
+  background-color: ${Palette.BACKGROUND_CARD};
+  padding: 13px 20px;
+  margin-bottom: 16px;
+  svg {
+    cursor: pointer;
+  }
+  @media (max-width: 420px) {
+    display: flex;
+    position: sticky;
+    top: 0;
+    z-index: 1;
   }
 `;

d43a2d72491a9ede17cf6483a385affccf37c836
HWANBINYOO <dngh0825@naver.com>
2023-03-15 23:05:58 +0900

    selfStudy api error

--- src/api/selfStudy.ts
+++ src/api/selfStudy.ts
@@ -8,15 +8,18 @@ export const applySelfStudy = async (role: string) => {
 		toast.success('자습 신청이 완료 되었어요');
 		return { data };
 	} catch (e: any) {
-		if (e.message === 'Request failed with status code 409')
-			toast.warning('자습 신청 인원이 50명이 넘어 신청하실 수 없어요');
-		else toast.warning('자습 신청을 할 수 없는 상태에요');
+		if (e.message === 'Request failed with status code 202'){
+			toast.warning('자습신청할 수 있는 시간이 아니에요');
+		}
+		else if(e.message === 'Request failed with status code 409') {
+			toast.warning('자습 신청을 할 있는 상태가 아니에요');
+		}
 	}
 };
 
 export const applyCancelStudy = async (role: string) => {
 	try {
-		const { data } = await apiClient.put(SelfstudyController.cancelStudy(role));
+		const { data } = await apiClient.put(SelfstudyController.selfStudy(role));
 		toast.success('자습 신청이 취소 되었어요');
 		return { data };
 	} catch (e) {}
@@ -24,10 +27,17 @@ export const applyCancelStudy = async (role: string) => {
 
 export const applyModifyStudy = async (role: string, num:number) => {
 	try {
-		const { data } = await apiClient.put(SelfstudyController.modifyStudy(role,num), {
+		const { data } = await apiClient.put(SelfstudyController.studyModify(role,num), {
 			number:num,
 		});
 		toast.success('자습 인원이 수정 되었어요');
 		return { data };
 	} catch (e) {}
 };
+
+export const selfStudyInfo = async (role: string) => {
+	try {
+		const { data } = await apiClient.put(SelfstudyController.selfStudyInfo(role));
+		return { data };
+	} catch (e) {}
+}

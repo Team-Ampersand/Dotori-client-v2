02bfcc742f65e92741679af8ac92e879c541965b
HWANBINYOO <dngh0825@naver.com>
2023-03-16 22:10:17 +0900

    Update : PenaltyModal 기능구현

--- src/components/Home/molecules/PenaltyModal/index.tsx
+++ src/components/Home/molecules/PenaltyModal/index.tsx
@@ -2,23 +2,38 @@ import { XtextIcon } from "assets/svg";
 import ModalHeader from "components/Common/atoms/ModalHeader";
 import PenaltyItem from "components/Home/atoms/PenaltyItem";
 import { ModalOverayWrapper } from 'components/Common/atoms/Wrappers/ModalOverayWrapper/style';
-import { ModalProps } from "types";
+import { ModalProps, myProfileType, PenaltyItemType } from "types";
 import * as S from "./style";
+import useSWR from 'swr';
+import { MemberController, penaltyController } from "utils/Libs/requestUrls";
+import { penaltyModalState } from "recoilAtoms/recoilAtomContainer";
+import { useRecoilState } from "recoil";
 
-const PenaltyModal = ({modalState, setModalState}:ModalProps) => {
+const PenaltyModal = () => {
+    const [penaltyModal, setPenaltyModal] = useRecoilState(penaltyModalState);
+    const { data:ProfileData } = useSWR<myProfileType>(MemberController.myProfile);
+    const { data } = useSWR<PenaltyItemType[]>(penaltyController.strRule(ProfileData?.stuNum || ""));
+    
     return (
-        <ModalOverayWrapper isClick={modalState}>
+        <ModalOverayWrapper isClick={penaltyModal}>
             <S.PenaltyModalWrapper>
-                <ModalHeader name={"규정 위반 내역"} setState={setModalState} />
+                <ModalHeader name={"규정 위반 내역"} setState={setPenaltyModal} />
                 <S.PenaltyItems>
-                    <PenaltyItem name={"타호실 출입"} date={"2022-04-17"} />
-                    <PenaltyItem name={"타호실 출입"} date={"2022-04-17"} />
-                    <PenaltyItem name={"타호실 출입"} date={"2022-04-17"} />
-                    <PenaltyItem name={"타호실 출입"} date={"2022-04-17"} />
-                    <PenaltyItem name={"타호실 출입"} date={"2022-04-17"} />
-                    <PenaltyItem name={"타호실 출입"} date={"2022-04-17"} />
+                    {
+                        data ? (
+                            data?.map((i,idx) => (
+                                <PenaltyItem 
+                                    name={i.name} 
+                                    date={i.date}
+                                    key={idx} 
+                                />
+                            ))
+                        ):(
+                            <S.NullPenalty>규정위반 내역이 없습니다</S.NullPenalty>    
+                        )
+                    }
                 </S.PenaltyItems>
-            <S.PenaltyModalBtn onClick={() => setModalState(false)}>확인</S.PenaltyModalBtn>
+            <S.PenaltyModalBtn onClick={() => setPenaltyModal(false)}>확인</S.PenaltyModalBtn>
             </S.PenaltyModalWrapper>
         </ModalOverayWrapper>
     )

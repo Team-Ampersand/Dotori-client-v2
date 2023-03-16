39b8db666c2da8e0905f204c6ccc4832ccc3f2c4
HWANBINYOO <dngh0825@naver.com>
2023-03-16 22:09:34 +0900

    Fix : MenuModal

--- src/components/Home/molecules/MenuModal/index.tsx
+++ src/components/Home/molecules/MenuModal/index.tsx
@@ -1,12 +1,16 @@
 import MenuOption from "components/Home/atoms/MenuOption";
 import { useRouter } from "next/router";
 import { toast } from "react-toastify";
-import { MenuModal } from "types";
+import { MenuModal, myProfileType, PenaltyItemType } from "types";
 import { removeToken } from "utils/Libs/removeToken";
+import { MemberController, penaltyController } from "utils/Libs/requestUrls";
 import * as S from "./style";
+import useSWR from 'swr';
 
 const MenuModal = ({isClick, setIsClick, setPenaltyModal, setProfileImgModal}:MenuModal) => {
     const router = useRouter();
+    const { data:ProfileData } = useSWR<myProfileType>(MemberController.myProfile);
+    const { data } = useSWR<PenaltyItemType[]>(penaltyController.strRule(ProfileData?.stuNum || ""));
     const  menuArr: ('프로필수정'|'규정위반 내역'|'비밀번호 변경'|'로그아웃')[] = ['프로필수정', '규정위반 내역', '비밀번호 변경', '로그아웃'];
     
     const handleClick = (name:'프로필수정'|'규정위반 내역'|'비밀번호 변경'|'로그아웃') => {
@@ -15,12 +19,17 @@ const MenuModal = ({isClick, setIsClick, setPenaltyModal, setProfileImgModal}:Me
             case '프로필수정' : 
                 return setProfileImgModal(true);
             case '규정위반 내역' :
-                return setPenaltyModal(true);
+                return handlePenltyModalClick();
             case '비밀번호 변경' :
                 return router.push('/changePasswd');
             case '로그아웃' :
                 return logout();
         }
+    }
+
+    const handlePenltyModalClick = () => {
+        if(data) return setPenaltyModal(true)
+        else return toast.info('규정 위반 내역이 없습니다.!')
     }
 
     const logout = () => {

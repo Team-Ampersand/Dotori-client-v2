6934ffb756869c75777a15a27a63c697d6a41d53
HWANBINYOO <dngh0825@naver.com>
2023-03-16 11:09:26 +0900

    Add : PromotionPage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import { MainTemplates } from 'components/Common/templates/MainTemplates/style'
 import UseThemeEffect from 'hooks/useThemeEffect'
+import PromotionOganisms from 'components/Promotion/organisms/PromotionOganisms';
 
 const PromotionPage = () => {
   UseThemeEffect()
   return (
-    <>
-    </>
+    <MainTemplates>
+      <PromotionOganisms/>
+    </MainTemplates>
   )
 }
 

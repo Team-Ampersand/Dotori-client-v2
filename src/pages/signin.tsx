1b1d6a0f355d9d7d5078b67482ea95b83b66914c
HWANBINYOO <dngh0825@naver.com>
2023-03-18 13:20:12 +0900

    Update : signin, signUp Page

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,5 +1,7 @@
 import { SignInForm } from "components";
 import { AuthTemplates } from "components/Common/templates/AuthTemplates/style";
+import { GetServerSideProps } from "next";
+import { getToken } from "utils/Libs/getToken";
 
 const SignInPage = () => {
   return (
@@ -7,6 +9,23 @@ const SignInPage = () => {
         <SignInForm />
     </AuthTemplates>
   )
+}
+
+export const  getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { Authorization } = await getToken(ctx);
+
+  if (Authorization) {
+    return {
+      redirect: {
+        destination: '/home',
+        permanent: false,
+      },
+    }
+  }
+  
+  return {
+    props: {},
+  };
 }
 
 export default SignInPage

c5d4d7838d43db13f13fae985954c794e09e399a
HWANBINYOO <dngh0825@naver.com>
2023-03-18 17:21:58 +0900

    Fix : 설명글 줄바꿈

--- src/components/Promotion/molecules/DecsPart/index.tsx
+++ src/components/Promotion/molecules/DecsPart/index.tsx
@@ -27,10 +27,9 @@ const DecsPart = ({isShow,setIsShow}: {isShow:boolean,setIsShow:(isShow:boolean)
                     를 담다.
                 </S.PomotionTitle>
                 <S.PomotionDesc>
-                    <p>DOTORI는 GSM 기숙사 관리 시스템으로, 학생들이 주체적으로 개발한 웹사이트입니다.</p>
-                    <p>소프트웨어 마이스터고등학교 &엔퍼센트 동아리 부원들이 직접 개발하여,</p>
-                    <p>효율적인 서비스 제공을 목적으로 합니다. 이 작은 서비스가 우리 학교의 인식을 바꿀 좋은</p>
-                    <p>마케팅 거리가 될 수 있도록 만들어졌습니다.</p>
+                    <p>DOTORI는 GSM 기숙사 관리 시스템으로, 학생들이 주체적으로 개발한 웹사이트입니다. 소프트웨어</p>
+                    <p>마이스터고등학교 &엔퍼센트 동아리 부원들이 직접 개발하여, 효율적인 서비스 제공을 목적으로 합니다.</p>
+                    <p>이 작은 서비스가 우리 학교의 인식을 바꿀 좋은 마케팅 거리가 될 수 있도록 만들어졌습니다.</p>
                 </S.PomotionDesc>
                 <S.SignInBtn>
                     <Link href={"/signup"}>

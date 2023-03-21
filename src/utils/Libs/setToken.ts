3eea207432723b618014d73720c8795767d6714b
HWANBINYOO <dngh0825@naver.com>
2023-03-22 00:29:04 +0900

    Update : setToken : Bearer 추가해서 저장, 만료시간

--- src/utils/Libs/setToken.ts
+++ src/utils/Libs/setToken.ts
@@ -1,7 +1,17 @@
-import { GetServerSidePropsContext } from "next";
-import { setCookie } from 'nookies'
+import { GetServerSidePropsContext } from 'next';
+import { setCookie } from 'nookies';
 
-export const setToken = (Authorization:string, RefreshToken:string , ctx:GetServerSidePropsContext|null): void => {
-    setCookie(ctx, 'Authorization', Authorization, {maxAge: 10800,path: '/',}) // 3시간
-    setCookie(ctx, 'RefreshToken', RefreshToken, {maxAge: 604800,path: '/',}) // 일주일
-}
+export const setToken = (
+  Authorization: string,
+  RefreshToken: string,
+  ctx: GetServerSidePropsContext | null
+): void => {
+  setCookie(ctx, 'Authorization', `Bearer ${Authorization}`, {
+    maxAge: 10800,
+    path: '/',
+  }); // 3시간
+  setCookie(ctx, 'RefreshToken', `Bearer ${RefreshToken}`, {
+    maxAge: 15768000,
+    path: '/',
+  }); // 6개월
+};

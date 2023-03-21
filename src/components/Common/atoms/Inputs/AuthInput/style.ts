6a4f48feb9ed36984e81be0f15b76eb89673ec17
HWANBINYOO <dngh0825@naver.com>
2023-03-21 14:33:00 +0900

    Fix : AuthInput css

--- src/components/Common/atoms/Inputs/AuthInput/style.ts
+++ src/components/Common/atoms/Inputs/AuthInput/style.ts
@@ -1,17 +1,19 @@
-import styled from "@emotion/styled";
-import { Palette } from "styles/globals";
-import { AuthInputStyleProps } from "types";
+import styled from '@emotion/styled';
+import { Palette } from 'styles/globals';
+import { AuthInputStyleProps } from 'types';
 
 export const AuthInput = styled.div`
   position: relative;
   height: 55px;
-  width: ${((props:AuthInputStyleProps) => props.isRabel ? `385px` : props.isEmailAuth ? `255px` : `385px`)};
+  width: ${(props: AuthInputStyleProps) =>
+    props.isRabel ? `385px` : props.isEmailAuth ? `255px` : `385px`};
   display: flex;
   justify-content: center;
   align-items: center;
 
-  input{
-    width: ${((props:AuthInputStyleProps) => props.isRabel ? `280px` : props.isEmailAuth ? `215px` : `340px`)};
+  input {
+    width: ${(props: AuthInputStyleProps) =>
+      props.isRabel ? `280px` : props.isEmailAuth ? `215px` : `340px`};
     height: 52px;
     display: flex;
     align-items: center;
@@ -22,37 +24,44 @@ export const AuthInput = styled.div`
     font-size: 16px;
     border: none;
     border-radius: 8px;
-    padding: 0 ${((props:AuthInputStyleProps) => props.isRabel ? `50px` : `20px`)};
+    padding: 0
+      ${(props: AuthInputStyleProps) => (props.isRabel ? `50px` : `20px`)};
 
-    ::placeholder{
+    ::placeholder {
       color: ${Palette.NEUTRAL_N30};
     }
-    :focus{
+    :focus {
       border: 1px solid ${Palette.PRIMARY_P10};
       color: ${Palette.NEUTRAL_N10};
     }
   }
-  input[type="number"]::-webkit-outer-spin-button,
-  input[type="number"]::-webkit-inner-spin-button {
+  input[type='number']::-webkit-outer-spin-button,
+  input[type='number']::-webkit-inner-spin-button {
     -webkit-appearance: none;
     margin: 0;
   }
-  svg{
+  svg {
     position: absolute;
-    top:15px;
-    right:10px;
+    top: 15px;
+    right: 13px;
     cursor: pointer;
-  } 
+  }
 
-  .frontIcon{
-    left: 10px;
+  .frontIcon {
+    left: 16px;
   }
 
   @media (max-width: 420px) {
-    width: ${((props:AuthInputStyleProps) => props.isRabel ? `90vw` : props.isEmailAuth ? `70vw` : `90vw`)};
+    width: ${(props: AuthInputStyleProps) =>
+      props.isRabel ? `90vw` : props.isEmailAuth ? `70vw` : `90vw`};
     input {
-      width: ${((props:AuthInputStyleProps) => props.isRabel ? `calc(90vw - 100px)` : props.isEmailAuth ? `50vw` : `calc(90vw - 40px)`)};
+      width: ${(props: AuthInputStyleProps) =>
+        props.isRabel
+          ? `calc(90vw - 100px)`
+          : props.isEmailAuth
+          ? `50vw`
+          : `calc(90vw - 40px)`};
       margin: 0 auto;
     }
-  } 
+  }
 `;

7f05e422bd5edb29337e39689aed806b3f8ef22a
HWANBINYOO <dngh0825@naver.com>
2023-03-23 00:14:10 +0900

    Fix : getRole

--- src/utils/Libs/getRole.ts
+++ src/utils/Libs/getRole.ts
@@ -2,16 +2,19 @@ import { parseCookies } from 'nookies';
 import jwt from 'jwt-decode';
 import { RoleType } from 'types';
 import { GetServerSidePropsContext } from 'next';
+import GET_ROLE from 'assets/json/role.json';
 
-export const getRole = (ctx?: GetServerSidePropsContext) => {
+function getUserToken(ctx?: GetServerSidePropsContext) {
   let token = ctx
     ? ctx.req.cookies['Authorization']
     : parseCookies().Authorization;
   if (!token) return '';
   const user: RoleType = jwt(token.replace('Bearer', ''));
-  if (user.roles[0] === 'ROLE_MEMBER') return 'member';
-  else if (user.roles[0] === 'ROLE_ADMIN') return 'admin';
-  else if (user.roles[0] === 'ROLE_DEVELOPER') return 'developer';
-  else if (user.roles[0] === 'ROLE_COUNCILLOR') return 'councillor';
-  else return '';
+  return user.roles[0];
+}
+
+export const getRole = (ctx?: GetServerSidePropsContext) => {
+  const user = getUserToken(ctx);
+  if (!user) return;
+  return GET_ROLE[user];
 };

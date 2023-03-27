70998e235f33fafae24573ede70ea9bfe5c4fbca
kangkong <kang3530554@gmail.com>
2023-03-27 20:40:05 +0900

    Remove useless code

--- src/pages/notice/[boardId].tsx
+++ src/pages/notice/[boardId].tsx
@@ -1,7 +1,6 @@
 import SideBar from 'components/Common/organisms/Sidebar';
 import { MainTemplates } from 'components/Common/templates/MainTemplates/style';
 import NoticeContent from 'components/Notice/organisms/NoticeContent';
-import NoticeEdit from 'components/Notice/organisms/NoticeEdit';
 import NoticeHeader from 'components/Notice/organisms/NoticeHeader';
 import NoticeList from 'components/Notice/organisms/NoticeList';
 import NoticeWrite from 'components/Notice/organisms/NoticeWrite';
@@ -11,15 +10,13 @@ import {
 } from 'components/Notice/templates/NoticeTemplate/style';
 import UseThemeEffect from 'hooks/useThemeEffect';
 import { GetServerSideProps, NextPage } from 'next';
-import { useRecoilValue, useSetRecoilState } from 'recoil';
-import { isNoticeWrite, noticeContent } from 'recoilAtoms/recoilAtomContainer';
+import { useRecoilValue } from 'recoil';
+import { isNoticeWrite } from 'recoilAtoms/recoilAtomContainer';
 import { SWRConfig } from 'swr';
 import { noticePageProps } from 'types';
 import { noticeDetailType } from 'types/components/NoticePage';
-import { apiClient } from 'utils/Libs/apiClient';
 import { getRole } from 'utils/Libs/getRole';
 import { getToken } from 'utils/Libs/getToken';
-import { NoticeController } from 'utils/Libs/requestUrls';
 
 const Notice: NextPage<{
   fallback: Record<string, noticePageProps> | Record<string, noticeDetailType>;
@@ -48,22 +45,6 @@ const Notice: NextPage<{
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const { Authorization } = await getToken(ctx);
   const role = await getRole(ctx);
-  const boardId = ctx.params?.boardId ?? '';
-
-  const { data: noticeData } = await apiClient.get(
-    NoticeController.getNotice(role),
-    { headers: { Authorization } }
-  );
-
-  const { data: noticeDetailData } = await apiClient.get(
-    NoticeController.getNoticeDetail(role, boardId[0]),
-    { headers: { Authorization } }
-  );
-
-  // const { data: deleteNotice } = await apiClient.delete(
-  //   NoticeController.getNoticeDetail(role, boardId[0]),
-  //   { headers: { Authorization } }
-  // );
 
   if (!Authorization) {
     return {
@@ -74,16 +55,9 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
     };
   }
 
-  if (noticeDetailData) {
-  }
-
   return {
     props: {
-      fallback: {
-        [NoticeController.getNotice(role)]: noticeData,
-        // [NoticeController.getNoticeDetail(role, boardId[0])]: noticeDetailData,
-        // [NoticeController.getNoticeDetail(role, boardId[0])]: deleteNotice,
-      },
+      fallback: {},
       role,
     },
   };

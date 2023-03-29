77b1440af7d609261a7979cac1ec6de6b770f6f4
HWANBINYOO <dngh0825@naver.com>
2023-03-29 17:38:43 +0900

    Add : RoleData

---
+++ src/assets/data/RoleData.ts
@@ -0,0 +1,21 @@
+import { Palette } from 'styles/globals';
+import { RoleDataType } from 'types';
+
+export const RoleData: RoleDataType = {
+  TOKEN: {
+    ROLE_DEVELOPER: 'developer',
+    ROLE_COUNCILLOR: 'councillor',
+    ROLE_ADMIN: 'admin',
+    ROLE_MEMBER: 'member',
+  },
+  WRITER: {
+    ROLE_DEVELOPER: '도토리',
+    ROLE_COUNCILLOR: '기숙사자치위원회',
+    ROLE_ADMIN: '사감선생님',
+  },
+  WRITERCOLOR: {
+    ROLE_DEVELOPER: `${Palette.PRIMARY_P10}`,
+    ROLE_COUNCILLOR: `${Palette.SUB_RED}`,
+    ROLE_ADMIN: `${Palette.SUB_YELLOW}`,
+  },
+};

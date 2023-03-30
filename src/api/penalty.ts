a922e1e33d7161bef7790ba4d50ce68df5c23cfc
Jeongmin <srm0609@naver.com>
2023-03-30 23:13:01 +0900

    Add: penalty api

---
+++ src/api/penalty.ts
@@ -0,0 +1,76 @@
+import { AxiosResponse } from 'axios';
+import { PenaltyRuleItemType } from 'types';
+import { apiClient } from 'utils/Libs/apiClient';
+import { penaltyController } from 'utils/Libs/requestUrls';
+
+export const getPenaltyList = async (
+  role: string,
+  memberName: string,
+  stuNum: string
+) => {
+  try {
+    const { data } = await apiClient.get(penaltyController.strRule(role), {
+      params: { memberName: memberName, stuNum: stuNum },
+    });
+    return { data };
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const getPenaltyRule = async (role: string, stuNum: string) => {
+  try {
+    const { data }: AxiosResponse<PenaltyRuleItemType> = await apiClient.get(
+      penaltyController.studentRule(role, stuNum)
+    );
+    return { data };
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const recordPenalty = async (
+  role: string,
+  stuNum: string[],
+  rule: string[],
+  date: string
+) => {
+  try {
+    await apiClient.post(penaltyController.strRule(role), {
+      stuNum: stuNum,
+      rule: rule,
+      date: date,
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const deletePenatly = async (role: string, rule_id: number) => {
+  try {
+    await apiClient.delete(penaltyController.deletePenalty(role, rule_id));
+    return true;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const selfPenaltySearch = async (
+  role: string,
+  name: string | null,
+  grade: string | null,
+  classNum: string | null,
+  gender: string | null
+) => {
+  try {
+    const { data } = await apiClient.get(penaltyController.strRule(role), {
+      params: {
+        name: name,
+        grade: grade,
+        classNum: classNum,
+        gender: gender,
+      },
+    });
+    return { data };
+  } catch (e: any) {}
+};

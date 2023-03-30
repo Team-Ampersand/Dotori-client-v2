4813cf794236cbe280f6efa9e4a1da43fa28a4b5
Jeongmin <srm0609@naver.com>
2023-03-30 23:07:16 +0900

    Update: todayDate => getDate

---
+++ src/utils/getDate.ts
@@ -0,0 +1,9 @@
+export const getDate = (props = new Date()) => {
+  let date = props;
+  const year = String(date.getFullYear()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  const hour = String(date.getHours()).padStart(2, '0');
+  const minute = String(date.getMinutes()).padStart(2, '0');
+  return [year, month, day, hour, minute];
+};

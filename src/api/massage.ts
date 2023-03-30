ffedbeea818b52d5f508f5c3d0959800b9745871
HWANBINYOO <dngh0825@naver.com>
2023-03-30 10:09:40 +0900

    fix : message, selfStudy api

--- src/api/massage.ts
+++ src/api/massage.ts
@@ -4,13 +4,11 @@ import { MassageController } from 'utils/Libs/requestUrls';
 
 export const applyMassage = async (role: string) => {
   try {
-    const { data } = await apiClient.post(MassageController.massage(role));
-    if (data.code === 202) {
-      toast.error(data.msg);
-      return false;
-    }
-    toast.success('안마의자 신청이 완료되었어요');
-    return true;
+    const {
+      data: { code, msg },
+    } = await apiClient.post(MassageController.massage(role));
+    if (code === 202) return !toast.error(msg);
+    return toast.success('안마의자 신청이 완료되었어요');
   } catch (e: any) {
     if (e.message === 'Request failed with status code 409')
       toast.warning('안마의자 신청을 할 수 없는 상태에요');
@@ -20,9 +18,11 @@ export const applyMassage = async (role: string) => {
 
 export const applyCancelMassage = async (role: string) => {
   try {
-    await apiClient.delete(MassageController.massage(role));
-    toast.success('안마의자 신청이 취소 되었어요.');
-    return true;
+    const {
+      data: { code, msg },
+    } = await apiClient.delete(MassageController.massage(role));
+    if (code === 202) return !toast.error(msg);
+    return toast.success('안마의자 신청이 취소 되었어요.');
   } catch (e) {
     return false;
   }
@@ -33,8 +33,7 @@ export const applyModifyMassage = async (role: string, limit: number) => {
     await apiClient.patch(MassageController.modifyMassage(role), {
       limit,
     });
-    toast.success('안마의자 인원이 수정 되었어요');
-    return true;
+    return toast.success('안마의자 인원이 수정 되었어요');
   } catch (e) {
     return false;
   }

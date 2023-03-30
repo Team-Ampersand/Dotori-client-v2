fd34434c15aace02ad1f38744798c8f0a2ab8781
Jeongmin <srm0609@naver.com>
2023-03-30 23:32:58 +0900

    Update: penaltyList

--- src/components/Penalty/molecules/PenaltyList/index.tsx
+++ src/components/Penalty/molecules/PenaltyList/index.tsx
@@ -2,173 +2,31 @@ import * as S from './style';
 import PenaltyItem from 'components/Penalty/atoms/PenaltyItem';
 import PenaltyListModal from '../PenaltyListModal';
 import PenaltyRecordModal from '../PenaltyRecordModal';
-import { useState } from 'react';
-import { ModalProps } from 'types';
-
-const PenaltyList = ({ modalState, setModalState }: ModalProps) => {
-  const userOBJ = [
-    {
-      id: 1,
-      memberName: '선민재',
-      gender: 'MAN',
-      stuNum: '2215',
-      ruleBigViolationList: ['', '', ''],
-    },
-    {
-      id: 1,
-      memberName: '조재영',
-      gender: 'MAN',
-      stuNum: '2215',
-      ruleBigViolationList: ['', '', ''],
-    },
-    {
-      id: 1,
-      memberName: '박영재',
-      gender: 'MAN',
-      stuNum: '2215',
-      ruleBigViolationList: ['', '', ''],
-    },
-    {
-      id: 1,
-      memberName: '강경민',
-      gender: 'MAN',
-      stuNum: '2215',
-      ruleBigViolationList: ['', '', ''],
-    },
-    {
-      id: 1,
-      memberName: '김준',
-      gender: 'MAN',
-      stuNum: '2215',
-      ruleBigViolationList: ['', '', ''],
-    },
-    {
-      id: 1,
-      memberName: '전승원',
-      gender: 'MAN',
-      stuNum: '2215',
-      ruleBigViolationList: ['', '', ''],
-    },
-    {
-      id: 1,
-      memberName: '손정민',
-      gender: 'MAN',
-      stuNum: '2215',
-      ruleBigViolationList: ['', '', ''],
-    },
-    {
-      id: 1,
-      memberName: '김시훈',
-      gender: 'MAN',
-      stuNum: '2215',
-      ruleBigViolationList: ['', '', ''],
-    },
-    {
-      id: 1,
-      memberName: '유환빈',
-      gender: 'MAN',
-      stuNum: '2215',
-      ruleBigViolationList: ['', '', ''],
-    },
-    {
-      id: 1,
-      memberName: '이름',
-      gender: 'MAN',
-      stuNum: '2215',
-      ruleBigViolationList: ['', '', ''],
-    },
-    {
-      id: 1,
-      memberName: '이름',
-      gender: 'MAN',
-      stuNum: '2215',
-      ruleBigViolationList: ['', '', ''],
-    },
-    {
-      id: 1,
-      memberName: '이름',
-      gender: 'MAN',
-      stuNum: '2215',
-      ruleBigViolationList: ['', '', ''],
-    },
-    {
-      id: 1,
-      memberName: '이름',
-      gender: 'MAN',
-      stuNum: '2215',
-      ruleBigViolationList: ['', '', ''],
-    },
-    {
-      id: 1,
-      memberName: '이름',
-      gender: 'MAN',
-      stuNum: '2215',
-      ruleBigViolationList: ['', '', ''],
-    },
-    {
-      id: 1,
-      memberName: '이름',
-      gender: 'MAN',
-      stuNum: '2215',
-      ruleBigViolationList: ['', '', ''],
-    },
-    {
-      id: 1,
-      memberName: '이름',
-      gender: 'MAN',
-      stuNum: '2215',
-      ruleBigViolationList: ['', '', ''],
-    },
-    {
-      id: 1,
-      memberName: '이름',
-      gender: 'MAN',
-      stuNum: '2215',
-      ruleBigViolationList: ['', '', ''],
-    },
-    {
-      id: 1,
-      memberName: '이름',
-      gender: 'MAN',
-      stuNum: '2215',
-      ruleBigViolationList: ['', '', ''],
-    },
-    {
-      id: 1,
-      memberName: '이름',
-      gender: 'MAN',
-      stuNum: '2215',
-      ruleBigViolationList: ['', '', ''],
-    },
-  ];
-  const [penaltyListModal, setPenaltyListModal] = useState(false);
+import { useRecoilState } from 'recoil';
+import {
+  penaltyList,
+  penaltyListModalState,
+  penaltyRecordModalState,
+} from 'recoilAtoms/recoilAtomContainer';
+const PenaltyList = () => {
+  const penaltyListModal = useRecoilState(penaltyListModalState);
+  const penaltyRecordModal = useRecoilState(penaltyRecordModalState);
+  const [penaltyOBJ] = useRecoilState(penaltyList);
 
   return (
     <S.PenaltyListWrapper>
-      {userOBJ &&
-        userOBJ.map((i, idx) => (
+      {penaltyOBJ &&
+        penaltyOBJ.map((i, idx) => (
           <PenaltyItem
             key={idx}
-            setPenaltyListModal={setPenaltyListModal}
-            setPenaltyRecordModal={setModalState}
             name={i.memberName}
             gender={i.gender}
             number={i.stuNum}
-            ruleBigViolationList={i.ruleBigViolationList}
+            ruleList={i.rule}
           />
         ))}
-      {penaltyListModal && (
-        <PenaltyListModal
-          modalState={penaltyListModal}
-          setModalState={setPenaltyListModal}
-        />
-      )}
-      {modalState && (
-        <PenaltyRecordModal
-          modalState={modalState}
-          setModalState={setModalState}
-        />
-      )}
+      {penaltyListModal && <PenaltyListModal />}
+      {penaltyRecordModal && <PenaltyRecordModal />}
     </S.PenaltyListWrapper>
   );
 };

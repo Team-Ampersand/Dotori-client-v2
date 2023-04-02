34fd40f574747aafa1991428374c93c0c419d39a
HWANBINYOO <dngh0825@naver.com>
2023-04-03 02:58:30 +0900

    Fix : AuthInputProps

--- src/types/Inputs.ts
+++ src/types/Inputs.ts
@@ -1,14 +1,21 @@
-import { InputHTMLAttributes } from "react";
-import { UseFormRegisterReturn } from "react-hook-form";
+import { InputHTMLAttributes } from 'react';
+import { UseFormRegisterReturn } from 'react-hook-form';
 
-export interface AuthInputProps extends React.DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>{
-    register: UseFormRegisterReturn;
-    labelName?:string;
-    isValue:boolean;
-    isEmailAuth?:boolean;
-    DeleteBtnClick: ()=> void;
+export interface AuthInputProps
+  extends React.DetailedHTMLProps<
+    InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement
+  > {
+  register: UseFormRegisterReturn;
+  labelName?: string;
+  isValue: boolean;
+  isEmailAuth?: boolean;
+  isEmail?: boolean;
+  DeleteBtnClick: () => void;
 }
 export interface AuthInputStyleProps {
-	isRabel: boolean;
-    isEmailAuth: boolean
-};
+  isRabel?: boolean;
+  isEmailAuth?: boolean;
+  isValue?: boolean;
+  isFocus?: boolean;
+}

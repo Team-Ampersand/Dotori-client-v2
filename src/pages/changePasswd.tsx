58e6eaa4214035537e1b43b488cec311fc3ab331
HWANBINYOO <dngh0825@naver.com>
2023-04-03 00:13:59 +0900

    Fix : ChangePasswdPage

--- src/pages/changePasswd.tsx
+++ src/pages/changePasswd.tsx
@@ -2,34 +2,31 @@ import ChangePasswdForm from 'components/ChangePasswd/organisms/ChangePasswdForm
 import PasswdEmailCheck from 'components/ChangePasswd/organisms/PasswdEmailCheck';
 import SEOHead from 'components/Common/atoms/SEOHead';
 import { AuthTemplates } from 'components/Common/templates/AuthTemplates/style';
-import { GetServerSideProps, NextPage } from 'next';
 import { useRecoilState } from 'recoil';
 import { isemailPasswordCheck } from 'recoilAtoms/recoilAtomContainer';
 import { isNotNull } from 'utils/isNotNull';
 import { getToken } from 'utils/Libs/getToken';
 
-const ChangePasswdPage: NextPage<{ isLogin: boolean }> = ({ isLogin }) => {
+const initIsLogin = async () => {
+  const { Authorization } = await getToken(null);
+  return isNotNull(Authorization);
+};
+
+const ChangePasswdPage = () => {
+  const isLogin = initIsLogin();
   const [IsemailPasswordCheck] = useRecoilState(isemailPasswordCheck);
   return (
     <>
       <SEOHead title={'| 비밀번호변경페이지'} />
       <AuthTemplates>
         {isLogin || IsemailPasswordCheck.isAuth ? (
-          <ChangePasswdForm isLogin={isLogin} />
+          <ChangePasswdForm isLogin={isLogin as unknown as boolean} />
         ) : (
           <PasswdEmailCheck />
         )}
       </AuthTemplates>
     </>
   );
-};
-
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const { Authorization } = await getToken(ctx);
-  const isLogin = isNotNull(Authorization);
-  return {
-    props: { isLogin },
-  };
 };
 
 export default ChangePasswdPage;

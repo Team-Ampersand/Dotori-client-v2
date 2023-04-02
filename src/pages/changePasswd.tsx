131697c870408bf8d0e797297ab5857023ce69b0
HWANBINYOO <dngh0825@naver.com>
2023-04-03 00:59:32 +0900

    Fix : ChangePasswdPage isLogin

--- src/pages/changePasswd.tsx
+++ src/pages/changePasswd.tsx
@@ -2,25 +2,22 @@ import ChangePasswdForm from 'components/ChangePasswd/organisms/ChangePasswdForm
 import PasswdEmailCheck from 'components/ChangePasswd/organisms/PasswdEmailCheck';
 import SEOHead from 'components/Common/atoms/SEOHead';
 import { AuthTemplates } from 'components/Common/templates/AuthTemplates/style';
+import { parseCookies } from 'nookies';
 import { useRecoilState } from 'recoil';
 import { isemailPasswordCheck } from 'recoilAtoms/recoilAtomContainer';
 import { isNotNull } from 'utils/isNotNull';
-import { getToken } from 'utils/Libs/getToken';
-
-const initIsLogin = async () => {
-  const { Authorization } = await getToken(null);
-  return isNotNull(Authorization);
-};
 
 const ChangePasswdPage = () => {
-  const isLogin = initIsLogin();
+  const { Authorization } = parseCookies();
   const [IsemailPasswordCheck] = useRecoilState(isemailPasswordCheck);
+  const isLogin = isNotNull(Authorization);
+
   return (
     <>
       <SEOHead title={'| 비밀번호변경페이지'} />
       <AuthTemplates>
         {isLogin || IsemailPasswordCheck.isAuth ? (
-          <ChangePasswdForm isLogin={isLogin as unknown as boolean} />
+          <ChangePasswdForm isLogin={isLogin} />
         ) : (
           <PasswdEmailCheck />
         )}

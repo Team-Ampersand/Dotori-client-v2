14c0c016dae287c6859180b8ccdce578b5105ac5
HWANBINYOO <dngh0825@naver.com>
2023-04-02 18:34:17 +0900

    Fix : SEOHead

--- src/components/Common/atoms/SEOHead/index.tsx
+++ src/components/Common/atoms/SEOHead/index.tsx
@@ -3,16 +3,20 @@ import Head from 'next/head';
 const SEOHead = ({ title }: { title: string }) => {
   return (
     <Head>
-      <title>{`DOTORI | ${title}`}</title>
+      <title>{`DOTORI ${title}`}</title>
       <meta
         name="description"
         content="GSM 기숙사 관리 웹 서비스 Dotori 입니다."
       />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta property="og:title" content={`DOTORI | ${title}`} />
+      <meta
+        name="og:description"
+        content="GSM 기숙사 관리 웹 서비스 Dotori 입니다."
+      />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta property="og:type" content="website" />
-      <meta property="og:image" content="/favicon.ico" />
+      <meta property="og:image" content="assets/png/Dotori3D.png" />
     </Head>
   );
 };

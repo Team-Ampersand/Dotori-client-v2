cefc922c8ca7dadb6da1b4665df7f4c65c8d9334
kangkong <kang3530554@gmail.com>
2023-04-02 19:00:17 +0900

    ✨ Add onDelete modal

--- src/components/Notice/molecules/ContentHeader/index.tsx
+++ src/components/Notice/molecules/ContentHeader/index.tsx
@@ -1,7 +1,9 @@
 import { deleteNotice } from 'api/notice';
 import { RoleData } from 'assets/data/RoleData';
 import { EditPencilIcon, TrashCan } from 'assets/svg';
+import CommonCheckModal from 'components/Common/molecules/CommonCheckModal';
 import { useRouter } from 'next/router';
+import { useState } from 'react';
 import { useSetRecoilState } from 'recoil';
 import { isNoticeModify, isNoticeWrite } from 'recoilAtoms/recoilAtomContainer';
 import { mutate } from 'swr';
@@ -20,12 +22,21 @@ const ContentHeader = ({ data }: props) => {
   const router = useRouter();
   const setNoticeWrite = useSetRecoilState(isNoticeWrite);
   const setNoticeModify = useSetRecoilState(isNoticeModify);
+  const [modal, setModal] = useState(false);
   const createdDate = new Date(data.createdDate);
   const writeDate = `${getDate(createdDate)[0]}년 ${
     getDate(createdDate)[1]
   }월 ${getDate(createdDate)[2]}일 ${getDate(createdDate)[3]}시 ${
     getDate(createdDate)[4]
   }분`;
+
+  const onDelete = async () => {
+    setNoticeWrite(false);
+    setModal(false);
+    data && (await deleteNotice(role, data.id));
+    mutate(NoticeController.getNotice(role));
+    router.push('/notice');
+  };
 
   return (
     <S.ContentHeader>
@@ -49,18 +60,18 @@ const ContentHeader = ({ data }: props) => {
           >
             <EditPencilIcon />
           </button>
-          <button
-            onClick={async () => {
-              setNoticeWrite(false);
-              data && (await deleteNotice(role, data.id));
-              mutate(NoticeController.getNotice(role));
-              router.push('/notice');
-            }}
-          >
+          <button onClick={() => setModal(true)}>
             <TrashCan />
           </button>
         </S.IconBox>
       </S.HeaderRightBox>
+      <CommonCheckModal
+        title="공지사항 삭제"
+        content="정말로 해당 공지사항을 삭제하겠습니까?"
+        modalState={modal}
+        setModalState={setModal}
+        onClick={onDelete}
+      />
     </S.ContentHeader>
   );
 };

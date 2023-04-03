b4445527a399f8e82836395f709ce862e45c13cf
HWANBINYOO <dngh0825@naver.com>
2023-04-03 15:52:22 +0900

    Update : PromotionPage middleware

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,6 @@
 import UseThemeEffect from 'hooks/useThemeEffect';
 import { PromotionTemplates } from 'components/Promotion/templates/PromotionTemplates/style';
 import PromotionOrganisms from 'components/Promotion/organisms/PromotionOrganisms';
-import { GetServerSideProps } from 'next';
-import { getToken } from 'utils/Libs/getToken';
 import SEOHead from 'components/Common/atoms/SEOHead';
 
 const PromotionPage = () => {
@@ -15,23 +13,6 @@ const PromotionPage = () => {
       </PromotionTemplates>
     </>
   );
-};
-
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const { Authorization } = await getToken(ctx);
-
-  if (Authorization) {
-    return {
-      redirect: {
-        destination: '/home',
-        permanent: false,
-      },
-    };
-  }
-
-  return {
-    props: {},
-  };
 };
 
 export default PromotionPage;

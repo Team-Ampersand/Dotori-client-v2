1d544f42ae550d90f94a2008844dc806e3a51011
HWANBINYOO <dngh0825@naver.com>
2023-04-04 00:30:56 +0900

    Fix : SideBar : dynamic import

--- src/components/Common/organisms/Sidebar/index.tsx
+++ src/components/Common/organisms/Sidebar/index.tsx
@@ -1,15 +1,22 @@
 import { DotoriIcon, DotoriLogo } from 'assets/svg';
 import LogoutButton from 'components/Common/atoms/Buttons/LogoutButton';
-import SideBarList from 'components/Common/molecules/SidebarList';
 import { useRouter } from 'next/router';
 import { useMemo, useState } from 'react';
 import * as S from './style';
 import Link from 'next/link';
 import { MenuData } from 'assets/data/SidebarMenuData';
+import { getRole } from 'utils/Libs/getRole';
+import dynamic from 'next/dynamic';
 
-const SideBar = ({ role }: { role: string }) => {
+const SideBarList = dynamic(
+  () => import('../../../Common/molecules/SidebarList'),
+  { ssr: false }
+);
+
+const SideBar = () => {
   const { pathname } = useRouter();
   const [currentRouter, setCurrentRouter] = useState(pathname);
+  const role = getRole();
 
   const mappingSidebarMenu = useMemo(
     () => (

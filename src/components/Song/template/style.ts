fa9e617468432103e104570ec9c76db7a7219dd3
kangkong <kang3530554@gmail.com>
2023-04-04 20:50:09 +0900

    💄 Fix song layer width for sidebar width

--- src/components/Song/template/style.ts
+++ src/components/Song/template/style.ts
@@ -2,12 +2,20 @@ import styled from '@emotion/styled';
 
 export const SongTemplate = styled.div`
   padding: 0 72px;
-  width: 100%;
+  width: calc(100% - 240px);
   height: 100%;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   gap: 1rem;
+
+  @media (max-width: 1634px) {
+    width: calc(100% - 72px);
+  }
+  @media (max-width: 420px) {
+    width: 100%;
+    padding: 0 1.25rem;
+  }
 `;
 
 export const SongLayer = styled.div`

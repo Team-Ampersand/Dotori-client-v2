9b1b1f14a53e3d75bf8b612c6f503ca39b9e2ca1
HWANBINYOO <dngh0825@naver.com>
2023-04-06 09:22:14 +0900

    Fix : MenuOption design

--- src/components/Home/atoms/MenuOption/index.tsx
+++ src/components/Home/atoms/MenuOption/index.tsx
@@ -1,14 +1,12 @@
-import { MenuOptionProps } from "types";
-import { ChangeProfileModalcon } from "utils/ChangeProfileModalcon";
-import * as S from "./style";
+import { MenuOptionProps } from 'types';
+import { ChangeProfileModalcon } from 'utils/ChangeProfileModalcon';
+import * as S from './style';
 
-const MenuOption = ({name, onClick}:MenuOptionProps) => (
-    <S.MenuOptionWrapper onClick={onClick} name={name}>
-        <>
-            {ChangeProfileModalcon()[name]()}
-        </>
-        {name}
-    </S.MenuOptionWrapper>
-)
+const MenuOption = ({ name, onClick, theme }: MenuOptionProps) => (
+  <S.MenuOptionWrapper onClick={onClick} name={name} theme={theme}>
+    <>{ChangeProfileModalcon()[name]()}</>
+    {name}
+  </S.MenuOptionWrapper>
+);
 
 export default MenuOption;

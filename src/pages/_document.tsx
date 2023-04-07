d9ab704bfbdef1c6896efecb80cb230af32dfa21
HWANBINYOO <dngh0825@naver.com>
2023-04-07 12:02:59 +0900

    Update : Document darkmode

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,11 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
+const themeInitializerScript = `
+  (function () {
+    document.body.dataset.theme = document.cookie.match('(^|;)?themeCookie=([^;]*)(;|$)')[2] || (window.matchMedia?.('(prefers-color-scheme: dark)').matches ? "dark" : "light");
+  })();
+`;
+
 export default function Document() {
   return (
     <Html lang="ko">
@@ -7,6 +13,7 @@ export default function Document() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>
+        <script dangerouslySetInnerHTML={{ __html: themeInitializerScript }} />
         <Main />
         <NextScript />
       </body>

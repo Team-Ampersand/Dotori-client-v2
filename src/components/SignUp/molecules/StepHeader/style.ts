9cbe8fb3b699f4c504aaba20ce364702a822f0b3
HWANBINYOO <dngh0825@naver.com>
2023-05-16 21:06:17 +0900

    Fix : .prettierrc

--- src/components/SignUp/molecules/StepHeader/style.ts
+++ src/components/SignUp/molecules/StepHeader/style.ts
@@ -1,22 +1,21 @@
-import styled from "@emotion/styled";
+import styled from '@emotion/styled';
 
 export const StepHeaderWrapper = styled.div`
-    width: 415px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding-right: 27px;
+  width: 415px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 27px;
 
-    @media (max-width: 420px) {
-        width: 100vw;
-    }
-    
+  @media (max-width: 420px) {
+    width: 100vw;
+  }
 `;
 
 export const DotsWrapper = styled.div`
-    width: 48px;
-    height: 10px;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
+  width: 48px;
+  height: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;

c7c7243f08ab75b84f7afbcb68d484c7ddcf6d54
HWANBINYOO <dngh0825@naver.com>
2023-05-18 17:51:28 +0900

    Add : CommonPageWrapper

---
+++ src/components/Common/atoms/Wrappers/CommonPageWrapper/style.ts
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+export const CommonPageWrapper = styled.div`
+  width: calc(100% - 240px);
+  height: 100vh;
+  padding: 0 3vw 3vh 3vw;
+  @media (max-width: 1634px) {
+    padding: 0 2.5vw 2.5vh 2.5vw;
+    width: 100%;
+  }
+  @media (max-width: 420px) {
+    padding: 0 5vw 3vh 5vw;
+    gap: 1.5vh;
+  }
+`;

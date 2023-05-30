2ec78864f68e74985c3614ce4bbc51011a7d4c9c
kangkong0805 <kang3530554@gmail.com>
2023-05-30 10:41:39 +0900

    ♻️ Refactor: Song page 코드 리펙토링

--- src/components/Song/template/style.ts
+++ src/components/Song/template/style.ts
@@ -1,23 +1,5 @@
 import styled from '@emotion/styled';
 
-export const SongTemplate = styled.div`
-  padding: 0 72px 56px 72px;
-  width: calc(100% - 240px);
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  gap: 1rem;
-
-  @media (max-width: 1634px) {
-    width: calc(100% - 72px);
-  }
-  @media (max-width: 420px) {
-    width: 100%;
-    padding: 0 1.25rem;
-  }
-`;
-
 export const SongLayer = styled.div`
   width: 100%;
   height: calc(100% - 130px);

8f143fd7b6adce00f5b4ddf5f77731b5b573035d
HWANBINYOO <dngh0825@naver.com>
2023-06-29 20:53:56 +0900

    Fix : 신청을 할 수 없는상태일때 toast 로만 출력

--- src/components/Home/organisms/SelfStudyBoard/index.tsx
+++ src/components/Home/organisms/SelfStudyBoard/index.tsx
@@ -12,6 +12,7 @@ import { getRole } from 'utils/Libs/getRole';
 import useSWR from 'swr';
 import { SelfstudyController } from 'utils/Libs/requestUrls';
 import UseToggleTheme from 'hooks/useToggleTheme';
+import { toast } from 'react-toastify';
 
 const SelfStudyBoard = () => {
   const [info, setInfo] = useState<applyStyleProps>({
@@ -42,19 +43,19 @@ const SelfStudyBoard = () => {
   const handleApplyBtnClick = () => {
     switch (info.applyStatus) {
       case '자습신청':
-      case '신청불가':
         return StudyControll();
       case '신청취소':
         return setCheckModal(true);
       case '인원수정':
         return setApplyModifyModal(true);
+      case '신청불가':
+        return toast.error('자습신청을 할 수 없는 상태에요');
     }
   };
 
   const StudyControll = async (n?: number) => {
     switch (info.applyStatus) {
       case '자습신청':
-      case '신청불가':
         return (await applySelfStudy(role)) && mutate();
       case '신청취소':
         return (await applyCancelStudy(role)) && mutate();

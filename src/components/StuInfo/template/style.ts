502d5d54e7c42168c66712f19fa74ebe8f3333e2
강경민 <87346613+kangkong0805@users.noreply.github.com>
2023-07-03 11:31:08 +0900

    💄 Update: padding 값 수정

--- src/components/StuInfo/template/style.ts
+++ src/components/StuInfo/template/style.ts
@@ -3,7 +3,7 @@ import styled from '@emotion/styled';
 export const StuInfoTemplate = styled.div`
   width: calc(100% - 240px);
   height: 100%;
-  padding: 0 2.5vw;
+  padding: 0 3vw;
 
   @media (max-width: 1634px) {
     width: calc(100% - 2.5vw);

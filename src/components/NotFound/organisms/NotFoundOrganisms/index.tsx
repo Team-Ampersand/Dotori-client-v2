64c8b830a6264d923d69432868ac4dca84cb8068
HWANBINYOO <dngh0825@naver.com>
2023-07-06 21:16:44 +0900

    Fix : NotFoundOrganism

--- src/components/NotFound/organisms/NotFoundOrganisms/index.tsx
+++ src/components/NotFound/organisms/NotFoundOrganisms/index.tsx
@@ -1,34 +1,31 @@
 import Image from 'next/image';
 import * as S from './style';
 import NotFoundImg from 'assets/png/404.png';
+import Custom500Img from 'assets/png/500.png';
 import { useRouter } from 'next/router';
-import Custom500Text from 'components/Common/atoms/Items/Custom500Text/Custom500Text';
 
 const NotFoundOrganism = ({ statusCode }: { statusCode: number }) => {
   const router = useRouter();
   return (
     <S.NotFoundWrapper>
-      {statusCode === 404 ? (
+      {statusCode === 500 ? (
         <Image src={NotFoundImg} alt="404이미지" width={500} height={290} />
       ) : (
-        <Custom500Text />
+        <Image src={Custom500Img} alt="500이미지" width={500} height={250} />
       )}
       <S.NotFoundDecs>
-        {statusCode === 404 ? (
+        {statusCode === 500 ? (
           <>
             <h2>요청하신 페이지를 찾을수 없습니다.</h2>
             <h5>입력한 주소가 맞는지 다시 한번 확인해 주시기를 바랍니다.</h5>
           </>
         ) : (
           <>
-            <h2>지금 이 서비스와 연결할 수 없습니다.</h2>
-            <h5>
-              문제를 해결하기 위해 열심히 노력하고 있습니다 잠시 후 다시
-              확인해주세요.
-            </h5>
+            <h2>죄송합니다. 문제가 발생했습니다.</h2>
+            <h5>문제를 해결하고 있습니다. 잠시 후에 다시 시도해 주세요.</h5>
           </>
         )}
-        {statusCode === 404 && (
+        {statusCode === 500 && (
           <S.HomeUrlBtn onClick={() => router.back()}>이전화면</S.HomeUrlBtn>
         )}
       </S.NotFoundDecs>

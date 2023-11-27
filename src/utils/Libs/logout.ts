6d52fd4a1529bf8e90007ea46962cebcd78e4233
한준 <qw2661807@gmail.com>
2023-11-27 19:57:42 +0900

    :recycle: Refactor logout

--- src/utils/Libs/logout.ts
+++ src/utils/Libs/logout.ts
@@ -1,10 +1,8 @@
 import { toast } from 'react-toastify';
 import { mutate } from 'swr';
-import { removeToken } from 'utils/Libs/removeToken';
-
+import { removeToken } from './removeToken';
 export const logout = () => {
   removeToken();
   mutate(() => true, undefined, { revalidate: false });
-  removeToken();
   toast.success('로그아웃되었습니다.');
 };

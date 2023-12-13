1af575384df4407ee6e4909d00fc4b1c2bc80b9b
한준 <qw2661807@gmail.com>
2023-12-13 20:17:33 +0900

    Revert ":art: Change middleware config"
    This reverts commit 04a19e30988b785bfa345e1b854edea5f9b2ca96.

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ import { removeToken } from 'utils/Libs/removeToken';
 export const middleware = async (req: NextRequest) => {
   const ua = userAgent(req);
   const RefreshToken = req.cookies.get('RefreshToken');
-  const isAuthPage = ['/'].some((i) => i == req.nextUrl.pathname);
+  const isAuthPage = ['/signin', '/'].some((i) => i == req.nextUrl.pathname);
 
   if (RefreshToken && isAuthPage) {
     return NextResponse.redirect(new URL('/home', req.url));
@@ -20,6 +20,7 @@ export const middleware = async (req: NextRequest) => {
 
 export const config = {
   matcher: [
+    '/signin',
     '/home',
     '/selfstudy',
     '/massage',

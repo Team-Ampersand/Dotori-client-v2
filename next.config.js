080f1be86772e5b8c2950038dd0ad8188dc2155f
mini-aron <aron0613@naver.com>
2024-05-01 23:42:35 +0900

    📦️Add: @next/bundle-analyzer package

--- next.config.js
+++ next.config.js
@@ -23,3 +23,8 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+module.exports = withBundleAnalyzer();

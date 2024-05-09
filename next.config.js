8e3085a19a1ed781d6c3af83b6e1665265f19810
이운린 <105215297+likegitman@users.noreply.github.com>
2024-05-09 15:29:32 +0900

    :bug: Fix duplication export

--- next.config.js
+++ next.config.js
@@ -22,9 +22,8 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
-
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
-module.exports = withBundleAnalyzer();
+
+module.exports = withBundleAnalyzer(nextConfig);

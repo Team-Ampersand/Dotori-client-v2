f1b0f0fd9233f509a715bf2f83c8c9682a8faf5d
이아론 <aron0613@naver.com>
2024-08-10 23:21:47 +0900

    🔥 Delete: remove duplicate code

--- src/utils/Libs/logout.ts
+++ src/utils/Libs/logout.ts
@@ -5,6 +5,5 @@ import { removeToken } from 'utils/Libs/removeToken';
 export const logout = () => {
   removeToken();
   mutate(() => true, undefined, { revalidate: false });
-  removeToken();
   toast.success('로그아웃되었습니다.');
 };

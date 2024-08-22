0cbff99b2d2c60e56dd0d7ac34e7c8d4090536b4
Ethen1264 <jxorhks86909763@gmail.com>
2024-08-22 22:54:00 +0900

    🔥Delete: delete unnecessary token verification

--- src/utils/Libs/getToken.ts
+++ src/utils/Libs/getToken.ts
@@ -6,15 +6,6 @@ export const getToken = async (ctx: GetServerSidePropsContext | null) => {
   if (ctx) {
     let Authorization = ctx.req.cookies['Authorization'] || '';
     let RefreshToken = ctx.req.cookies['RefreshToken'] || '';
-
-    if (!RefreshToken) return {};
-    else if (!Authorization) {
-      const { newAuthorization }: any = await tokenReissue(RefreshToken, ctx);
-      if (newAuthorization) {
-        Authorization = newAuthorization;
-      }
-    }
-
     return { Authorization, RefreshToken };
   } else {
     const { Authorization, RefreshToken } = parseCookies();

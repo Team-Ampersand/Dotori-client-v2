71c828453ee97d97a1225265bd858024882ec209
Ethen1264 <jxorhks86909763@gmail.com>
2024-08-23 11:02:10 +0900

    🐛Fix: newAuthorization undefined Exception handling in progress

--- src/utils/Libs/getToken.ts
+++ src/utils/Libs/getToken.ts
@@ -6,6 +6,21 @@ export const getToken = async (ctx: GetServerSidePropsContext | null) => {
   if (ctx) {
     let Authorization = ctx.req.cookies['Authorization'] || '';
     let RefreshToken = ctx.req.cookies['RefreshToken'] || '';
+
+    if (!RefreshToken) return {};
+    if (!Authorization) {
+      try {
+        const response = await tokenReissue(RefreshToken, ctx);
+        if (response && response.newAuthorization) {
+          Authorization = response.newAuthorization;
+        } else {
+          return {};
+        }
+      } catch (error) {
+        return {};
+      }
+    }
+
     return { Authorization, RefreshToken };
   } else {
     const { Authorization, RefreshToken } = parseCookies();

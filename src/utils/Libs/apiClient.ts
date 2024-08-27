e1ab37cdf5f64582bf135b497f5185d829d7e22c
Ethen1264 <jxorhks86909763@gmail.com>
2024-08-27 19:13:09 +0900

    🐛Fix: set header token

--- src/utils/Libs/apiClient.ts
+++ src/utils/Libs/apiClient.ts
@@ -2,11 +2,14 @@ import axios from 'axios';
 import BASE_HEADER from '../Config/Config.json';
 import { tokenReissue } from 'api/member';
 import { getToken } from './getToken';
+import { setHeader } from './setHeader';
 
 export const apiClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
   headers: BASE_HEADER,
 });
+
+apiClient.interceptors.request.use(setHeader);
 
 apiClient.interceptors.response.use(
   function (response) {

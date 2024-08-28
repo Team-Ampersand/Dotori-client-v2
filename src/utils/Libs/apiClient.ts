c635474f0eca94de37b61a8e607d517adde42296
Ethen1264 <jxorhks86909763@gmail.com>
2024-08-28 10:19:56 +0900

    ✨Update: getRefresh

--- src/utils/Libs/apiClient.ts
+++ src/utils/Libs/apiClient.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 import BASE_HEADER from '../Config/Config.json';
-import { tokenReissue } from 'api/member';
-import { getToken } from './getToken';
 import { setHeader } from './setHeader';
+import { getRefresh } from './getRefresh';
 
 export const apiClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
@@ -11,37 +10,6 @@ export const apiClient = axios.create({
 
 apiClient.interceptors.request.use(setHeader);
 
-apiClient.interceptors.response.use(
-  function (response) {
-    return response;
-  },
-  async function (err) {
-    const originalRequest = err.config;
-
-    if (
-      err.response &&
-      (err.response.status === 401 || err.response.status === 403) &&
-      !originalRequest._retry
-    ) {
-      originalRequest._retry = true;
-
-      try {
-        const { RefreshToken } = await getToken(null);
-        const tokenResponse = await tokenReissue(
-          'Bearer ' + RefreshToken,
-          null
-        );
-
-        if (tokenResponse && tokenResponse.newAuthorization) {
-          originalRequest.headers['Authorization'] =
-            'Bearer ' + tokenResponse.newAuthorization;
-          return apiClient(originalRequest);
-        }
-      } catch (refreshError) {
-        return Promise.reject(refreshError);
-      }
-    }
-
-    return Promise.reject(err);
-  }
-);
+apiClient.interceptors.response.use(function (response) {
+  return response;
+}, getRefresh);
